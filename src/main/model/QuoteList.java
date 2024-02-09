ee8f378826a6a600dcf1703a081a2f6a0f4b0b4b
Kelly Hum <kellyhum88@gmail.com>
2024-02-08 17:33:24 -0800

    Add fields and methods

--- src/main/model/QuoteList.java
+++ src/main/model/QuoteList.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class QuoteList {
+    private ArrayList<String> quoteList;
+    private Scanner scan;
+
+    // EFFECTS: constructs a new QuoteList object with an empty
+    // quote list and scanner object for getting user input
+    public QuoteList() {
+        // setup
+        quoteList = new ArrayList<String>();
+        scan = new Scanner(System.in);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a user inputted quote to the end of the quote list
+    public void addQuote(Quote q) {
+        System.out.println("Type the quote that you want to add:");
+        String inputQuote = scan.nextLine();
+
+        quoteList.add(inputQuote);
+    }
+
+    public ArrayList<String> getQuoteList() {
+        return quoteList;
+    }
 }
