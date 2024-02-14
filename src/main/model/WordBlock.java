6e8115e861ca396b8b84c8fba10336dde01a664d
Kelly Hum <kellyhum88@gmail.com>
2024-02-14 10:10:20 -0800

    Add getters and setters; x y positioning

--- src/main/model/WordBlock.java
+++ src/main/model/WordBlock.java
@@ -3,16 +3,36 @@ package model;
 /* A single word in a quote */
 
 public class WordBlock {
-    private String word;
+    private final String word;
+    private int x1;
+    private int y1;
 
     // EFFECTS: constructs a new WordBlock object with a word value of the
     // inputted word
-    public WordBlock(String word) {
+    public WordBlock(String word, int x, int y) {
         this.word = word;
+        this.x1 = x;
+        this.y1 = y;
     }
 
-    // getter
+    // getters
     public String getWord() {
         return this.word;
+    }
+
+    public int getX() {
+        return this.x1;
+    }
+
+    public int getY() {
+        return this.y1;
+    }
+
+    public void setX(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setY(int y) {
+        this.y1 = y;
     }
 }
