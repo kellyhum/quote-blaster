04c31e807afb47f6617dd1507eefc8894af97455
Kelly Hum <kellyhum88@gmail.com>
2024-02-14 10:17:47 -0800

    Modify constructor and add some method bodies

--- src/main/model/Quote.java
+++ src/main/model/Quote.java
@@ -4,32 +4,39 @@ package model;
    a level of the game */
 
 public class Quote {
-    private String quote;
+    private final String quoteText;
     private boolean isTheCurrentLevel;
+    private boolean deleteQuote;
 
-    // EFFECTS: constructs a Quote object with the quote being the contents parameter value
-    public Quote(String quote) {
-        this.quote = quote;
-        this.isTheCurrentLevel = false;
+    // EFFECTS: constructs a Quote object with the quote being the quote parameter value, and
+    // whether or not the user wants to play the quote
+    public Quote(String quote, boolean isTheCurrentLevel) {
+        quoteText = quote;
+        this.isTheCurrentLevel = isTheCurrentLevel;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the value of deleteQuote to true
     public void setDeleteQuote() {
+        this.deleteQuote = true;
     }
 
     // MODIFIES: this
     // EFFECTS: toggles the value of the currentlyPlayingQuote value
     public void setIsTheCurrentLevel() {
-
+        this.isTheCurrentLevel = !this.isTheCurrentLevel;
     }
 
     // getters
-    public String getQuote() {
-        return quote;
+    public String getQuoteText() {
+        return quoteText;
     }
 
-    public boolean isTheCurrentLevel() {
+    public boolean getDeleteQuote() {
+        return deleteQuote;
+    }
+
+    public boolean getTheCurrentLevel() {
         return isTheCurrentLevel;
     }
 }
--- src/main/model/QuoteList.java
+++ src/main/model/QuoteList.java
@@ -1,26 +1,27 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /* A list of quotes that the user can play */
 
 public class QuoteList {
-    private ArrayList<String> quoteList;
-    private Scanner scan;
+    private final ArrayList<Quote> quoteList;
 
-    // EFFECTS: constructs a new QuoteList object with two
-    // quotes in the quoteList and initializes a scanner object for getting user input
+    // EFFECTS: constructs a new QuoteList object with two Quotes in the quoteList
     public QuoteList() {
+        quoteList = new ArrayList<>();
+        quoteList.add(new Quote("All's fair in love and war", false));
+        quoteList.add(new Quote("Curiosity killed the cat", false));
     }
 
     // MODIFIES: this
     // EFFECTS: adds a user inputted quote to the end of the quote list
     public void addQuote(Quote q) {
+        quoteList.add(q);
     }
 
     // getter
-    public ArrayList<String> getQuoteList() {
+    public ArrayList<Quote> getQuoteList() {
         return quoteList;
     }
 }
