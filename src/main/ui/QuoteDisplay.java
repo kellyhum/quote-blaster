9b79c11ad6af9eb0e32e8fd2bbc6d4801cde1413
Kelly Hum <kellyhum88@gmail.com>
2024-03-05 17:13:20 -0800

    Add ability to save and load data

--- src/main/ui/QuoteDisplay.java
+++ src/main/ui/QuoteDisplay.java
@@ -2,19 +2,29 @@ package ui;
 
 import model.Quote;
 import model.QuoteList;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 /* displays the quote list and quotes */
 
 public class QuoteDisplay {
-    private final QuoteList quoteList;
+    private static final String JSON_FILE = "./data/test.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private QuoteList quoteList;
     private final Scanner scan;
 
-    // EFFECTS: intiialize Scanner and empty QuoteList
+    // EFFECTS: intiialize Scanner, empty QuoteList, and both JsonWriter and JsonReader
     public QuoteDisplay() {
         quoteList = new QuoteList();
         scan = new Scanner(System.in);
+        jsonWriter = new JsonWriter(JSON_FILE);
+        jsonReader = new JsonReader(JSON_FILE);
     }
 
     // EFFECTS: runs quote displaying and commands functions
@@ -38,6 +48,9 @@ public class QuoteDisplay {
     // EFFECTS: asks for user input
     public void quoteCommands() {
         System.out.println("\nPress A to add a new quote");
+        System.out.println("Press S to save the current quotelist");
+        System.out.println("Press L to load the previous quotelist");
+        System.out.println("Press V to view the quotelist");
         System.out.println("Press B to go back to the main menu");
 
         String userInput = scan.nextLine();
@@ -72,9 +85,38 @@ public class QuoteDisplay {
 
             printQuotes();
             quoteCommands();
+        } else if (u.equals("S")) {
+            saveQuoteList();
+            quoteCommands();
+        } else if (u.equals("L")) {
+            loadQuoteList();
+            quoteCommands();
+        } else if (u.equals("V")) {
+            printQuotes();
+            quoteCommands();
         } else if (u.equals("B")) {
             // todo!!!
             return;
+        }
+    }
+
+    public void saveQuoteList() {
+        try {
+            jsonWriter.open();
+            jsonWriter.writeQuoteList(quoteList);
+            jsonWriter.close();
+            System.out.println("Saved to " + JSON_FILE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_FILE);
+        }
+    }
+
+    public void loadQuoteList() {
+        try {
+            this.quoteList = jsonReader.readQuoteList();
+            System.out.println("Loaded from " + JSON_FILE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_FILE);
         }
     }
 
