dfd52b175f20fd490dc5d4f654dfe67caf2107de
Kelly Hum <kellyhum88@gmail.com>
2024-03-05 17:12:35 -0800

    Add method to convert quote to json

--- src/main/model/Quote.java
+++ src/main/model/Quote.java
@@ -3,6 +3,8 @@ package model;
 /* A quote in the quote list, where a quote represents
    a level of the game */
 
+import org.json.JSONObject;
+
 public class Quote {
     private final String quoteText;
     private boolean isTheCurrentLevel;
@@ -25,6 +27,13 @@ public class Quote {
     // EFFECTS: toggles the value of the currentlyPlayingQuote value
     public void setIsTheCurrentLevel() {
         this.isTheCurrentLevel = !this.isTheCurrentLevel;
+    }
+
+    public JSONObject convertToJson() {
+        JSONObject json = new JSONObject();
+        json.put("quote", quoteText);
+        json.put("isCurrLevel", isTheCurrentLevel);
+        return json;
     }
 
     // getters
