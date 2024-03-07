641f3b2c2807ab86a40310dfdf6397f2faab6f27
Kelly Hum <kellyhum88@gmail.com>
2024-03-06 16:40:17 -0800

    Add game read and write functionality

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -2,6 +2,7 @@ package persistence;
 
 import model.Quote;
 import model.QuoteList;
+import model.WordBlock;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public class JsonReader {
@@ -16,12 +18,6 @@ public class JsonReader {
 
     public JsonReader(String sourceFile) {
         this.sourceFile = sourceFile;
-    }
-
-    public QuoteList readQuoteList() throws IOException {
-        String jsonData = readFile(sourceFile);
-        JSONObject jsonObject = new JSONObject(jsonData);
-        return parseQuoteList(jsonObject);
     }
 
     private String readFile(String source) throws IOException {
@@ -34,6 +30,24 @@ public class JsonReader {
         return contentBuilder.toString();
     }
 
+    public QuoteList readQuoteList() throws IOException {
+        String jsonData = readFile(sourceFile);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseQuoteList(jsonObject);
+    }
+
+    public ArrayList<WordBlock> readWordList() throws IOException {
+        String jsonData = readFile(sourceFile);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseWordList(jsonObject);
+    }
+
+    public int readAndParseScore() throws IOException {
+        String jsonData = readFile(sourceFile);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return jsonObject.getInt("score");
+    }
+
     private QuoteList parseQuoteList(JSONObject jsonObject) {
         QuoteList ql = new QuoteList();
         addQuoteToQuoteList(ql, jsonObject);
@@ -43,7 +57,7 @@ public class JsonReader {
     private void addQuoteToQuoteList(QuoteList ql, JSONObject jsonObject) {
         JSONArray jsonArray = jsonObject.getJSONArray("quoteDisplay");
         for (Object json : jsonArray) {
-            JSONObject nextQuote = (JSONObject) json; // get the json of the quote
+            JSONObject nextQuote = (JSONObject) json; // convert to json object
             initializeQuote(nextQuote, ql); // add quote to the quotelist
         }
     }
@@ -54,5 +68,28 @@ public class JsonReader {
 
         Quote q = new Quote(name, isCurrLevel);
         ql.addQuote(q);
+    }
+
+    private ArrayList<WordBlock> parseWordList(JSONObject jsonObject) {
+        ArrayList<WordBlock> wl = new ArrayList<>();
+        addWordToWordList(wl, jsonObject);
+        return wl;
+    }
+
+    private void addWordToWordList(ArrayList<WordBlock> wb, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("words");
+        for (Object json : jsonArray) {
+            JSONObject nextWord = (JSONObject) json; // convert to json object
+            initializeWord(nextWord, wb); // add quote to the quotelist
+        }
+    }
+
+    private void initializeWord(JSONObject jsonObject, ArrayList<WordBlock> wb) {
+        String word = jsonObject.getString("word");
+        int x1 = jsonObject.getInt("x");
+        int y1 = jsonObject.getInt("y");
+
+        WordBlock w = new WordBlock(word, x1, y1);
+        wb.add(w);
     }
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -2,16 +2,18 @@ package persistence;
 
 import model.Quote;
 import model.QuoteList;
+import model.WordBlock;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class JsonWriter {
     private String destPath;
     private PrintWriter printer;
-    private final static int TAB = 4;
+    private static final int TAB = 4;
 
     public JsonWriter(String destPath) {
         this.destPath = destPath;
@@ -21,6 +23,13 @@ public class JsonWriter {
     public void open() throws FileNotFoundException {
         printer = new PrintWriter(destPath);
     }
+
+//    public void writeAll(QuoteList ql, int score, ArrayList<WordBlock> wb) {
+//        JSONArray mainArray = new JSONArray();
+//        mainArray.put(writeQuoteList(ql));
+//
+//        mainArray.put(writeGameStats(score, wb));
+//    }
 
     public void writeQuoteList(QuoteList quoteList) {
         JSONArray quoteArray = new JSONArray();
@@ -35,6 +44,21 @@ public class JsonWriter {
         mainElement.put("quoteDisplay", quoteArray);
 
         // convert array to string, print the output
+        printer.print(mainElement.toString(TAB));
+    }
+
+    public void writeGameStats(int score, ArrayList<WordBlock> w) {
+        JSONObject mainElement = new JSONObject();
+        JSONArray wordList = new JSONArray();
+
+        for (WordBlock word : w) {
+            JSONObject wordObj = word.convertToJson();
+            wordList.put(wordObj);
+        }
+
+        mainElement.put("score", score);
+        mainElement.put("words", wordList);
+
         printer.print(mainElement.toString(TAB));
     }
 
