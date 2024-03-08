7e62297a4d02613cb3190463e5eb2c17d25981e6
Kelly Hum <kellyhum88@gmail.com>
2024-03-07 17:34:06 -0800

    Add tests

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,4 +1,95 @@
+/*
+    Code referenced from https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+ */
+
 package persistence;
 
-public class JsonReaderTest {
+import model.QuoteList;
+import model.WordBlock;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonReaderTest extends JsonTest {
+    @Test
+    public void testNonExistentFileQuote() {
+        JsonReader reader = new JsonReader("", "./data/noSuchFile.json");
+        try {
+            QuoteList ql = reader.readQuoteList();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void testNonExistentFileGame() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json", "");
+        try {
+            ArrayList<WordBlock> wb = reader.readWordList();
+            int score = reader.readAndParseScore();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void testEmptyQuoteList() {
+        JsonReader reader = new JsonReader("", "./data/emptyQuoteList.json");
+        try {
+            QuoteList ql = reader.readQuoteList();
+            assertTrue(ql.getQuoteList().isEmpty());
+        } catch (IOException e) {
+            fail("not supposed to throw");
+        }
+    }
+
+    @Test
+    public void testEmptyWordListAndScore() {
+        JsonReader reader = new JsonReader("./data/noWordsOrScore.json", "");
+        try {
+            ArrayList<WordBlock> wb = reader.readWordList();
+            int score = reader.readAndParseScore();
+            assertTrue(wb.isEmpty());
+            assertEquals(0, score);
+        } catch (IOException e) {
+            fail("not supposed to throw");
+        }
+    }
+
+    @Test
+    public void testQuoteList() {
+        JsonReader reader = new JsonReader("", "./data/normalQuoteList.json");
+        try {
+            QuoteList ql = reader.readQuoteList();
+            assertFalse(ql.getQuoteList().isEmpty());
+            assertEquals(2, ql.getQuoteList().size());
+            checkQuoteValues("lkjlkj", false, ql.getQuoteList().get(0));
+            checkQuoteValues("lk", true, ql.getQuoteList().get(1));
+        } catch (IOException e) {
+            fail("not supposed to throw");
+        }
+    }
+
+    @Test
+    public void testWordListAndScore() {
+        JsonReader reader = new JsonReader("./data/normalWordAndScore.json", "");
+        try {
+            ArrayList<WordBlock> wb = reader.readWordList();
+            int score = reader.readAndParseScore();
+            assertFalse(wb.isEmpty());
+            assertEquals(3, wb.size());
+            assertEquals(1, score);
+            checkWordValues("testBlock", 40, 5, wb.get(0));
+            checkWordValues("test", 23, 0, wb.get(1));
+            checkWordValues("quote", 18, 9, wb.get(2));
+        } catch (IOException e) {
+            fail("not supposed to throw");
+        }
+    }
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,4 +1,76 @@
+/*
+    Code referenced from https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+ */
+
 package persistence;
 
-public class JsonWriterTest {
+import model.Quote;
+import model.QuoteList;
+import model.WordBlock;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonWriterTest extends JsonTest {
+    @Test
+    public void testInvalidFile() {
+        try {
+            Quote q = new Quote("test quote", false);
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void testWriteQuoteList() {
+        try {
+            QuoteList ql = new QuoteList();
+            ql.addQuote(new Quote("test", true));
+
+            JsonWriter writer = new JsonWriter("./data/emptyQuoteListWriter.json");
+            writer.open();
+            writer.writeQuoteList(ql);
+            writer.close();
+
+            JsonReader reader = new JsonReader("", "./data/emptyQuoteListWriter.json");
+            QuoteList ql2 = reader.readQuoteList();
+            assertFalse(ql2.getQuoteList().isEmpty());
+            assertEquals(1, ql2.getQuoteList().size());
+            checkQuoteValues("test", true, ql2.getQuoteList().get(0));
+
+        } catch (IOException e) {
+            fail("should not have been thrown");
+        }
+    }
+
+    @Test
+    public void testWriteGame() {
+        try {
+            int score = 3;
+            ArrayList<WordBlock> wb = new ArrayList<>();
+            wb.add(new WordBlock("testing word", 10, 5));
+
+            JsonWriter writer = new JsonWriter("./data/noWordsOrScoreWriter.json");
+            writer.open();
+            writer.writeGameStats(score, wb);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/noWordsOrScoreWriter.json", "");
+            ArrayList<WordBlock> wb2 = reader.readWordList();
+            int score2 = reader.readAndParseScore();
+            assertFalse(wb2.isEmpty());
+            assertEquals(1, wb2.size());
+            assertEquals(3, score2);
+            checkWordValues("testing word", 10, 5, wb2.get(0));
+
+        } catch (IOException e) {
+            fail("should not have been thrown");
+        }
+    }
 }
