077c39db7eb451c096c98e93519b92f05d9cca1d
Kelly Hum <kellyhum88@gmail.com>
2024-03-21 16:11:55 -0700

    Modify color, adjust to reflect changes in quotedisplay

--- src/main/ui/ScoreDisplaySwing.java
+++ src/main/ui/ScoreDisplaySwing.java
@@ -1,29 +1,26 @@
 package ui;
 
 import model.Game;
-import model.Quote;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ScoreDisplaySwing extends JPanel {
     private Game game;
     private JLabel maxScore;
     private JButton quoteButton;
 
-    public ScoreDisplaySwing(Game game) {
-        setBackground(new Color(180, 180, 180));
-
-        this.game = game;
+    public ScoreDisplaySwing(Game g) {
+        setBackground(new Color(89, 141, 133));
+        this.game = g;
         quoteButton = new JButton("change quotes");
         quoteButton.setFocusable(false);
         quoteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("button clicked");
-                QuoteDisplaySwing quoteDisplay = new QuoteDisplaySwing();
-                game.setActiveQuoteList(quoteDisplay.getActiveQuoteList());
+                new QuoteDisplaySwing(game);
             }
         });
 
